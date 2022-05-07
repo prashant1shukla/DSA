126e2839897c0ac7834cf375400570a8d130fbf7
prashant1shukla <77752155+prashant1shukla@users.noreply.github.com>
2022-05-07 17:25:11 +0530

    Create tree.cpp

---
+++ tree.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int key;
+    struct Node *left;
+    struct Node *right;
+    Node(int k)
+    {
+        key =k;
+        left=NULL;
+        right=NULL;
+    }
+};
+void inorder(Node *root)
+{
+    if(root!=NULL)
+    {
+        inorder(root->left);
+        cout<<(root->key)<<" ";
+        inorder(root->right);
+    }
+}
+int height(Node *root)
+{
+    if(root==NULL)
+        return 0;
+    else
+        return (max(height(root->left),height(root->right))+1);
+}
+int main()
+{
+    Node *root=new Node(10);
+    root->left=new Node(20);
+    root->right=new Node(30);
+    root->left->left=new Node(40);
+    root->left->right=new Node(50);
+    inorder(root);
+    cout<<endl;
+    cout<<height(root)<<endl;
+}
