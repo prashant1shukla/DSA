80debf3f0de3ed38b6b25b4520bee83294ba97b0
prashant1shukla <77752155+prashant1shukla@users.noreply.github.com>
2022-06-18 18:04:11 +0530

    Binary search_striver_leetcode_new

---
+++ Tree/bottomview.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+    Node(int k)
+    {
+        data =k;
+        left=NULL;
+        right=NULL;
+    }
+};
+vector<int>v;
+  
+void bottom(Node *root,int hd,int lvl,map<int,pair<int,int>>&mp)
+{
+    if(root==NULL)
+        return;
+    if(mp.find(hd)==mp.end())
+        mp[hd]=make_pair(root->data,lvl);
+    else
+    {
+        pair < int, int > p = mp[hd];
+        if (p.second <= lvl)
+        {
+            mp[hd].second = lvl;
+            mp[hd].first = root -> data;
+        }
+    }
+    //if(root->left==NULL)
+    bottom(root->left,hd-1,lvl+1,mp);
+    //mp[hd]=(root->data);
+    //if(root->right!=NULL)
+    bottom(root->right,hd+1,lvl+1,mp);
+    // for(auto x:mp)
+    //         cout<<(x.second);
+}
+vector <int> bottomView(Node *root) {
+    v.clear();
+    map<int,pair<int,int>>mp;
+    mp.clear();
+    bottom(root,0,0,mp);
+    for(auto x:mp)
+        v.push_back(x.second.first);
+    return v;
+}
+int main()
+{
+    Node *root=new Node(10);
+    root->left=new Node(20);
+    root->right=new Node(30);
+    root->left->left=new Node(40);
+    root->left->right=new Node(50);
+
+    //printSpiral(root);
+
+    bottomView(root);
+    for(auto x:v)
+     cout<<x<<" ";  
+    
+
+    
+}
